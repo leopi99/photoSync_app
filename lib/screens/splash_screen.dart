b3c30324a89c68e0bed942441bc0279769c0b9f0
Leonardo Pizio <pizio.leonardo@gmailcom>
2021-05-29 16:31:24 +0200

    Added splash animation.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:photo_sync/global/nav_key.dart';
 import 'package:photo_sync/routes/route_builder.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,9 +12,17 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   static const Duration _animationDuration = Duration(seconds: 2);
 
+  late double opacity;
+
   @override
   void initState() {
     super.initState();
+    opacity = 0;
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      setState(() {
+        opacity = 1;
+      });
+    });
     Future.delayed(_animationDuration).then((_) => _onAnimationEnd());
   }
 
@@ -22,8 +31,14 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlutterLogo(
-          duration: _animationDuration,
+        child: AnimatedOpacity(
+          duration: Duration(milliseconds: 1500),
+          curve: Curves.ease,
+          opacity: opacity,
+          child: FlutterLogo(
+            size: MediaQuery.of(navigatorKey.currentContext!).size.width / 2,
+            style: FlutterLogoStyle.markOnly,
+          ),
         ),
       ),
     );
