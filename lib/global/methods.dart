6d43c773a884c951c0d2aa1046ef3f5e4f81096e
Pizio Leonardo <pizio.leonardo@gmail.com>
2021-05-30 20:17:07 +0200

    Update methods.dart

--- lib/global/methods.dart
+++ lib/global/methods.dart
@@ -4,8 +4,9 @@ import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:photo_sync/global/nav_key.dart';
 
 class GlobalMethods {
-  static Future<void> setStatusBarColorAsScaffoldBackground() async {
-    await FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+  static Future<void> setStatusBarColorAsScaffoldBackground(
+      {bool whiteForeground = false}) async {
+    await FlutterStatusbarcolor.setStatusBarWhiteForeground(whiteForeground);
     await FlutterStatusbarcolor.setStatusBarColor(
         Theme.of(navigatorKey.currentContext!).scaffoldBackgroundColor);
   }
