d0782454ca06864d587d63355b32b96f581118f1
Pizio Leonardo <pizio.leonardo@gmail.com>
2021-05-31 21:17:37 +0200

    Removed email from user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,14 +1,11 @@
 class User {
   final String username;
-  final String email;
 
   User({
     required this.username,
-    required this.email,
   });
 
   factory User.fromJSON(Map<String, dynamic> json) => User(
-        email: json['email'],
         username: json['username'],
       );
 }
