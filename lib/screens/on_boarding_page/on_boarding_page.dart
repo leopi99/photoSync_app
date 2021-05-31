670b9a1c3518845e7ee574b951c490d9c1ed3b79
Leonardo Pizio <pizio.leonardo@gmailcom>
2021-05-31 20:33:03 +0200

    Some changes to the onBoarding page

--- lib/screens/on_boarding_page/on_boarding_page.dart
+++ lib/screens/on_boarding_page/on_boarding_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:photo_sync/bloc/app_bloc.dart';
 import 'package:photo_sync/constants/assets_path.dart';
 import 'package:photo_sync/global/methods.dart';
+import 'package:photo_sync/inherited_widgets/appearance_bloc_inherited.dart';
 import 'package:photo_sync/models/on_boarding_info.dart';
 import 'package:photo_sync/util/enums/shared_type.dart';
 import 'package:photo_sync/util/shared_manager.dart';
@@ -50,35 +51,96 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        onPageChanged: (value) => setState(() {
-          currentPage = value;
-        }),
-        itemCount: pages.length,
-        controller: _pageController,
-        physics: BouncingScrollPhysics(),
-        pageSnapping: true,
-        itemBuilder: (context, index) {
-          return Center(
-            child: _buildPage(pages[index]),
-          );
-        },
-      ),
-      floatingActionButton: currentPage < pages.length - 1
-          ? FloatingActionButton.extended(
-              onPressed: () {
-                _pageController.animateToPage(currentPage + 1,
-                    duration: Duration(milliseconds: 250),
-                    curve: Curves.easeInBack);
-                //If the current page is the last, will update the ui to show the close button
-                if (currentPage == pages.length - 1) setState(() {});
-              },
-              label: Text('Next'),
-            )
-          : FloatingActionButton.extended(
-              onPressed: _goToHomepage,
-              label: Text('Close'),
+      body: Stack(
+        children: [
+          PageView.builder(
+            onPageChanged: (value) => setState(() {
+              currentPage = value;
+            }),
+            itemCount: pages.length,
+            controller: _pageController,
+            physics: BouncingScrollPhysics(),
+            pageSnapping: true,
+            itemBuilder: (context, index) {
+              return Center(
+                child: _buildPage(pages[index]),
+              );
+            },
+          ),
+          Align(
+            alignment: AlignmentDirectional.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 64),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: List.generate(
+                    pages.length, (index) => _buildStepper(index)),
+              ),
             ),
+          ),
+          Align(
+            alignment: AlignmentDirectional.bottomEnd,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 8, right: 8),
+              child: currentPage < pages.length - 1
+                  ? TextButton(
+                      onPressed: () {
+                        _pageController.animateToPage(currentPage + 1,
+                            duration: Duration(milliseconds: 250),
+                            curve: Curves.easeInBack);
+                        //If the current page is the last, will update the ui to show the close button
+                        // if (currentPage == pages.length - 1)
+                        setState(() {});
+                      },
+                      child: Text(
+                        'Next',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    )
+                  : TextButton(
+                      onPressed: _goToHomepage,
+                      child: Text(
+                        'Close',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+            ),
+          ),
+        ],
+      ),
+      // floatingActionButton: currentPage < pages.length - 1
+      //     ? FloatingActionButton.extended(
+      //         onPressed: () {
+      //           _pageController.animateToPage(currentPage + 1,
+      //               duration: Duration(milliseconds: 250),
+      //               curve: Curves.easeInBack);
+      //           //If the current page is the last, will update the ui to show the close button
+      //           // if (currentPage == pages.length - 1)
+      //           setState(() {});
+      //         },
+      //         label: Text('Next'),
+      //       )
+      //     : FloatingActionButton.extended(
+      //         onPressed: _goToHomepage,
+      //         label: Text('Close'),
+      //       ),
+    );
+  }
+
+  Widget _buildStepper(int index) {
+    return Container(
+      height: 10,
+      width: 10,
+      margin: EdgeInsets.symmetric(horizontal: 4),
+      decoration: BoxDecoration(
+        color: currentPage == index
+            ? AppearanceBlocInherited.of(context)
+                .appearance
+                .currentThemeData
+                .accentColor
+            : Colors.grey[300],
+        shape: BoxShape.circle,
+      ),
     );
   }
 
