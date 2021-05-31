9c0080c69036255aea383da18c6413fd9028921d
Pizio Leonardo <pizio.leonardo@gmail.com>
2021-05-31 22:50:15 +0200

    Added api not reachable image

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -3,4 +3,5 @@ class AssetsPath {
   static const String onBoardingImage2 = "assets/on_boarding/storage.svg";
   static const String onBoardingImage3 = "assets/on_boarding/no_work_needed.svg";
   static const String onBoardingImage4 = "assets/on_boarding/api_running.svg";
+  static const String notReachableApi = "assets/api_not_reachable.svg";
 }
