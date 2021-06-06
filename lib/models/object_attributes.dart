f2357406a1ec3d65a66b062c3956d49c3ce58a7b
Pizio Leonardo <pizio.leonardo@gmail.com>
2021-06-06 10:59:47 +0200

    Removed username field from the api response(was useless)

--- lib/models/object_attributes.dart
+++ lib/models/object_attributes.dart
@@ -2,7 +2,6 @@ class ObjectAttributes {
   static const _KEY_URL = "url";
   static const _KEY_SYNC_DATE = "sync_date";
   static const _KEY_CREATION_DATE = "creation_date";
-  static const _KEY_USERNAME = "user";
   static const _KEY_PICTURE_POSITION = "position";
   static const _KEY_BYTE_SIZE = "bytes_size";
   static const _KEY_LOCAL_PATH = "local_path";
@@ -13,7 +12,6 @@ class ObjectAttributes {
   final String url;
   final String syncDate;
   final String creationDate;
-  final String username;
   final String picturePosition;
   final String localPath;
   final bool isDownloaded;
@@ -24,7 +22,6 @@ class ObjectAttributes {
     required this.url,
     required this.syncDate,
     required this.creationDate,
-    required this.username,
     required this.picturePosition,
     required this.localPath,
     required this.pictureByteSize,
@@ -41,7 +38,6 @@ class ObjectAttributes {
         picturePosition: json[_KEY_PICTURE_POSITION],
         syncDate: json[_KEY_SYNC_DATE],
         url: json[_KEY_URL],
-        username: json[_KEY_USERNAME],
         databaseID: json[_KEY_ID],
       );
 
@@ -52,7 +48,6 @@ class ObjectAttributes {
         _KEY_PICTURE_POSITION: picturePosition,
         _KEY_SYNC_DATE: syncDate,
         _KEY_URL: url,
-        _KEY_USERNAME: username,
         _KEY_ID: databaseID,
         _KEY_IS_DOWNLOADED: isDownloaded,
       };
@@ -61,7 +56,6 @@ class ObjectAttributes {
     String? url,
     String? syncDate,
     String? creationDate,
-    String? username,
     String? picturePosition,
     String? localPath,
     bool? isDownloaded,
@@ -76,6 +70,5 @@ class ObjectAttributes {
         picturePosition: picturePosition ?? this.picturePosition,
         syncDate: syncDate ?? this.syncDate,
         url: url ?? this.url,
-        username: username ?? this.username,
       );
 }
