da8230c01bd427d6de1fe2d5704214ab0d732490
Pizio Leonardo <pizio.leonardo@gmail.com>
2021-06-17 20:55:12 +0200

    Sends the userID with the rawObject

--- lib/models/raw_object.dart
+++ lib/models/raw_object.dart
@@ -1,5 +1,6 @@
-import 'dart:convert';
 import 'dart:typed_data' show Int8List;
+import 'package:photo_sync/global/nav_key.dart';
+import 'package:photo_sync/inherited_widgets/auth_bloc_inherited.dart';
 import 'package:photo_sync/models/object.dart';
 
 class RawObject {
@@ -14,5 +15,8 @@ class RawObject {
   Map<String, dynamic> get toJSON => {
         'object': object.toJSON,
         'fileBytes': bytes.buffer.asUint8List().toList(),
+        'userID': AuthBlocInherited.of(navigatorKey.currentContext!)
+            .currentUser!
+            .userID,
       };
 }
